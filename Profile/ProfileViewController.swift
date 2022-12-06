f87a70a7e371a13e867dd18a06427142640d288f
Artem Yurchenko <yourchencore@yandex.ru>
2022-12-06 11:28:27 +0500

    create profile scene

---
+++ Profile/ProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Юрченко Артем on 06.12.2022.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    // MARK: - Public Properties
+    
+    // MARK: - Private Properties
+    
+    // MARK: - Lifecycle
+    // MARK: - Private Methods
+    // MARK: - IBActions
+    
+    @IBAction func didTapLogoutButton() {
+        
+    }
+    // MARK: - Table View Data Source
+}
