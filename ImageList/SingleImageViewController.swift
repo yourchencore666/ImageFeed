b0925a18aa0c1270989e4bfe276e2576917d30c6
Artem Yurchenko <yourchencore@yandex.ru>
2022-12-07 16:41:16 +0500

    create navigation controller

---
+++ ImageList/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Юрченко Артем on 06.12.2022.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+}
--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+final class ProfileViewController: UIViewController {
     
     // MARK: - IBOutlets
     @IBOutlet weak var profileImage: UIImageView!
